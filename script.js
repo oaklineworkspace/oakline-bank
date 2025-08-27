a8c16caa6afcdef7973de819175d08cc0ad9314b
oaklineworkspace <oaklineworkspace@gmail.com>
2025-08-26 21:07:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,22 @@
-// Testimonials Scroll Animation
-const testimonials = document.querySelectorAll('.testimonial');
-
-window.addEventListener('scroll', () => {
-  const triggerBottom = window.innerHeight / 5 * 4;
-
-  testimonials.forEach(testimonial => {
-    const testimonialTop = testimonial.getBoundingClientRect().top;
-
-    if(testimonialTop < triggerBottom) {
-      testimonial.style.opacity = '1';
-      testimonial.style.transform = 'translateY(0)';
-    }
-  });
-});
-
-// Section Pop-Up Animations
+// Scroll-triggered Pop-up and Testimonials Animation
 const popUps = document.querySelectorAll('.pop-up');
+const testimonials = document.querySelectorAll('.testimonial');
 
 window.addEventListener('scroll', () => {
   const triggerBottom = window.innerHeight / 5 * 4;
 
   popUps.forEach(section => {
     const sectionTop = section.getBoundingClientRect().top;
-
-    if(sectionTop < triggerBottom) {
+    if(sectionTop < triggerBottom){
       section.classList.add('active');
+    }
+  });
+
+  testimonials.forEach(testimonial => {
+    const testimonialTop = testimonial.getBoundingClientRect().top;
+    if(testimonialTop < triggerBottom){
+      testimonial.style.opacity = '1';
+      testimonial.style.transform = 'translateY(0)';
     }
   });
 });
