099231b6addcf55541e0de9612a0803a6ba10c7a
oaklineworkspace <oaklineworkspace@gmail.com>
2025-08-29 16:37:35 -0700

    Update updateEmailTemplates.js

--- Pages/api/updateEmailTemplates.js
+++ Pages/api/updateEmailTemplates.js
@@ -1,5 +1,6 @@
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
+  // Accept both GET and POST requests
+  if (req.method !== 'POST' && req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' })
   }
 
