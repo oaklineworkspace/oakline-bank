25eda95d530afe1ec60aea28a8790535f7c357ab
oaklineworkspace <oaklineworkspace@gmail.com>
2025-08-31 04:28:23 -0700

    Update account-management.js

--- pages/api/account-management.js
+++ pages/api/account-management.js
@@ -1,1 +1,9 @@
 
+export default function AccountManagement() {
+  return (
+    <div>
+      <h1>Account Management</h1>
+      <p>This is a placeholder page for managing user accounts.</p>
+    </div>
+  );
+}
