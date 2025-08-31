804dbbaf71420090ddaea066d41270daa6f6527f
oaklineworkspace <oaklineworkspace@gmail.com>
2025-08-31 04:48:01 -0700

    Create mfa-setup.js

---
+++ pages/mfa-setup.js
@@ -0,0 +1,8 @@
+export default function MfaSetup() {
+  return (
+    <div>
+      <h1>MFA Setup</h1>
+      <p>This is a placeholder for the multi-factor authentication setup page.</p>
+    </div>
+  );
+}
