8f29b60d49e9b2526fb45e4d7d939225550265c4
oaklineworkspace <oaklineworkspace@gmail.com>
2025-08-31 00:57:49 -0700

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -1,9 +1,1 @@
-// Hero.js
-export default function Hero() {
-  return (
-    <section>
-      <h1>Welcome to Oakline Bank</h1>
-      <p>Your trusted banking partner.</p>
-    </section>
-  );
-}
+
