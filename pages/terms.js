4eb9a8b41f76b2871a414afbad57816f0959bf96
oaklineworkspace <oaklineworkspace@gmail.com>
2025-08-31 04:53:15 -0700

    Create terms.js

---
+++ pages/terms.js
@@ -0,0 +1,8 @@
+export default function Terms() {
+  return (
+    <div>
+      <h1>Terms and Conditions</h1>
+      <p>This is a placeholder for the terms and conditions page.</p>
+    </div>
+  );
+}
