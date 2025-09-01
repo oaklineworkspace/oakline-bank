8a2334f7c7a55f6c5d4133b24e8d101592a42533
oaklineworkspace <oaklineworkspace@gmail.com>
2025-09-01 08:50:13 -0700

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -1,14 +1,18 @@
-import styles from '../styles/Hero.module.css';
-import Link from 'next/link';
+// components/Hero.js
+import styles from "../styles/Hero.module.css";
+import Link from "next/link";
 
-export default function Hero({ title, subtitle, ctaText, ctaLink }) {
+export default function Hero({ title, subtitle, ctaText, ctaHref, imgSrc }) {
   return (
     <section className={styles.hero}>
-      <h1>{title}</h1>
-      <p>{subtitle}</p>
-      <Link href={ctaLink}>
-        <a className={styles.ctaButton}>{ctaText}</a>
-      </Link>
+      <div className={styles.content}>
+        <h1>{title}</h1>
+        <p>{subtitle}</p>
+        <Link href={ctaHref}>
+          <button>{ctaText}</button>
+        </Link>
+      </div>
+      {imgSrc && <img src={imgSrc} alt={title} className={styles.image} />}
     </section>
   );
 }
