527afefb93431a14f97b91c9f859754fefdfcaae
oaklineworkspace <oaklineworkspace@gmail.com>
2025-09-01 05:11:40 -0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,16 @@
-import React from 'react';
+// components/Footer.js
+import styles from '../styles/Footer.module.css';
+import Link from 'next/link';
 
-const Footer = () => {
-  return <div>Footer placeholder</div>;
-};
-
-export default Footer;
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>&copy; {new Date().getFullYear()} Oakline Bank. All rights reserved.</p>
+      <div className={styles.links}>
+        <Link href="/privacy">Privacy Policy</Link>
+        <Link href="/terms">Terms & Conditions</Link>
+        <Link href="/support">Support</Link>
+      </div>
+    </footer>
+  );
+}
