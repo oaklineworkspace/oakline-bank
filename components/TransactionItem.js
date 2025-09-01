ec0bb4d203753d9473e153a071b717313c64f379
oaklineworkspace <oaklineworkspace@gmail.com>
2025-09-01 08:06:33 -0700

    Update TransactionItem.js

--- components/TransactionItem.js
+++ components/TransactionItem.js
@@ -2,7 +2,7 @@ import styles from '../styles/TransactionItem.module.css';
 
 export default function TransactionItem({ date, description, amount, type }) {
   return (
-    <div className={`${styles.transaction} ${type === 'income' ? styles.income : styles.expense}`}>
+    <div className={`${styles.transactionItem} ${type === 'income' ? styles.income : styles.expense}`}>
       <p className={styles.date}>{date}</p>
       <p className={styles.description}>{description}</p>
       <p className={styles.amount}>{amount}</p>
