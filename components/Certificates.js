385bd07295cd6bb81bbb4d7b8a7d4036235f5eb8
oaklineworkspace <oaklineworkspace@gmail.com>
2025-09-01 06:37:30 -0700

    Create Certificates.js

---
+++ components/Certificates.js
@@ -0,0 +1,13 @@
+export default function Certificates() {
+  return (
+    <section className="certificates-section">
+      <h2>Our Certifications & Partnerships</h2>
+      <div className="certificates-grid">
+        <img src="/images/cert1.png" alt="Certificate 1" />
+        <img src="/images/cert2.png" alt="Certificate 2" />
+        <img src="/images/cert3.png" alt="Certificate 3" />
+        <img src="/images/cert4.png" alt="Certificate 4" />
+      </div>
+    </section>
+  );
+}
