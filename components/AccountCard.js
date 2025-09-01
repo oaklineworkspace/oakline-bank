decaa1e43abb80f226b11d8efbfd049e4a8c05cf
oaklineworkspace <oaklineworkspace@gmail.com>
2025-09-01 05:16:27 -0700

    Update AccountCard.js

--- components/AccountCard.js
+++ components/AccountCard.js
@@ -1,7 +1,14 @@
-import React from 'react';
+// components/AccountCard.js
+import styles from '../styles/AccountCard.module.css';
 
-const AccountCard = () => {
-  return <div>AccountCard placeholder</div>;
-};
-
-export default AccountCard;
+export default function AccountCard({ type, benefits }) {
+  return (
+    <div className={styles.card}>
+      <h3>{type}</h3>
+      <ul>
+        {benefits.map((b, i) => <li key={i}>{b}</li>)}
+      </ul>
+      <a href="/create-account" className={styles.button}>Get Started</a>
+    </div>
+  );
+}
