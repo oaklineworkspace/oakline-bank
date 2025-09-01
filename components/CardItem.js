df1192327b8643a302324468d56af82db6cd5e2b
oaklineworkspace <oaklineworkspace@gmail.com>
2025-09-01 09:23:53 -0700

    Update CardItem.js

--- components/CardItem.js
+++ components/CardItem.js
@@ -1,12 +1,36 @@
-// components/CardItem.js
-import styles from '../styles/CardItem.module.css';
+// components/CardItems.js
+import styles from "../styles/CardItems.module.css";
 
-export default function CardItem({ title, description, icon }) {
+export default function CardItems({ cards }) {
   return (
-    <div className={styles.card}>
-      <img src={icon} alt={title} className={styles.icon} />
-      <h3>{title}</h3>
-      <p>{description}</p>
+    <div className={styles.cardGrid}>
+      {cards.map((card, index) => (
+        <div key={index} className={styles.card}>
+          <div className={styles.cardHeader}>
+            <img
+              src={card.logo}
+              alt={`${card.bank} logo`}
+              className={styles.bankLogo}
+            />
+            <span className={styles.bankName}>{card.bank}</span>
+          </div>
+
+          <div className={styles.cardNumber}>
+            **** **** **** {card.last4}
+          </div>
+
+          <div className={styles.cardDetails}>
+            <div>
+              <span className={styles.label}>Card Holder</span>
+              <p>{card.holder}</p>
+            </div>
+            <div>
+              <span className={styles.label}>Expiry</span>
+              <p>{card.expiry}</p>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
