b16c95a7c99fb983cfb4e9b2c3d278f7e1b37970
oaklineworkspace <oaklineworkspace@gmail.com>
2025-09-02 12:47:28 -0700

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,7 @@
-import Link from "next/link";
-
 export default function Header() {
   return (
-    <header style={{ padding: "20px", background: "#1E1E1E", color: "white", display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-      <h1>MySite</h1>
-      <nav>
-        <Link href="/" style={{ margin: "0 10px", color: "white" }}>Home</Link>
-        <Link href="/login" style={{ margin: "0 10px", color: "white" }}>Login</Link>
-        <Link href="/dashboard" style={{ margin: "0 10px", color: "white" }}>Dashboard</Link>
-      </nav>
+    <header style={{ padding: '1rem', background: '#f2f2f2', textAlign: 'center' }}>
+      <h1>Welcome to Oakline Bank</h1>
     </header>
-  );
+  )
 }
