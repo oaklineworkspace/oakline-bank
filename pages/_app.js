b2970fac5e8cdb8172d1b5a312a5ae8e17afda44
oaklineworkspace <oaklineworkspace@gmail.com>
2025-09-02 20:03:57 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 // pages/_app.js
-import '../styles/globals.css'; // Import your global stylesheet
+import '../styles/globals.css';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Oakline Bank</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Secure and convenient banking with Oakline Bank." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
-
-export default MyApp;
